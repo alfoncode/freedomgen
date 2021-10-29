d2bcf5d32af44c46a477cef702d8638dd9b9c61d
alfoncode <ajgarciaba@gmail.com>
2021-10-30 01:51:55 +0200

    Looking for!!

--- freedomgen.py
+++ freedomgen.py
@@ -11,14 +11,14 @@ def out(command):
 letras = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 vocales = ["a","e","i","o","u"]
 consonantes = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+custom = ["n","y","x"]
 
-for r1 in range(0,1):
-	for r2 in range(0,1):
+for r1 in range(0,len(consonantes)):
+	for r2 in range(0,len(vocales)):
 		for r3 in range(0,len(consonantes)):
-			for r4 in range(0,1):
-				for r5 in range(0,1):					
-					nombre = "deli" + consonantes[r3] + "ify"
-					if "aa" not in nombre and "ee" not in nombre and "ii" not in nombre and "oo" not in nombre and "uu" not in nombre:												
-						output = out("whois " + nombre + ".com|grep \"No match for domain\"" + ">> salida.txt")						
-						
+			for r4 in range(0,len(custom)):								
+				nombre = consonantes[r1] + vocales[r2] + consonantes[r3] + custom[r4]
+				if "aa" not in nombre and "ee" not in nombre and "ii" not in nombre and "oo" not in nombre and "uu" not in nombre:												
+					output = out("whois " + nombre + ".com|grep \"No match for domain\"" + ">> salida.txt")						
+					
 
